1611a924dcb6bc75a2feda4f16c39c7308ced451
Mijo Gracanin <mgracanin@rhapsody.com>
2015-12-15 12:25:35 +0100

    Initial Commit

---
+++ RemoveOptimizelyId/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RemoveOptimizelyId
+//
+//  Created by Mijo Gracanin on 15/12/15.
+//  Copyright © 2015 Mijo Gracanin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
