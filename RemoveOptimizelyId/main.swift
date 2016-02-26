e833e2ea7bc7daf8376ab9008567ac48bfea60b0
Mijo Gracanin <mgracanin@rhapsody.com>
2016-02-26 12:47:32 +0100

    Remove optimizelyId

--- RemoveOptimizelyId/main.swift
+++ RemoveOptimizelyId/main.swift
@@ -8,5 +8,34 @@
 
 import Foundation
 
-print("Hello, World!")
+let processDirectory = NSURL(fileURLWithPath:Process.arguments[0]).URLByDeletingLastPathComponent!.path!
+let projectDirectory = Process.arguments.count == 2 ? Process.arguments[1] : processDirectory
 
+let fm = NSFileManager.defaultManager()
+let files = fm.subpathsAtPath(projectDirectory)
+
+let regex1 = try NSRegularExpression(pattern:">\\s*<userDefinedRuntimeAttributes>\\s+<.*optimizelyId.*>\\s*<\\/userDefinedRuntimeAttributes>",
+    options: NSRegularExpressionOptions(rawValue: 0))
+
+let regex2 = try NSRegularExpression(pattern:">\\s*<userDefinedRuntimeAttribute\\s.*optimizelyId.*\\/>",
+    options: NSRegularExpressionOptions(rawValue: 0))
+
+for file in files! {
+    let path = projectDirectory + "/" + file
+    
+    if file.hasPrefix(".") || (!file.hasSuffix(".xib") && !file.hasSuffix(".storyboard")) {
+        continue
+    }
+    
+    var content = try String(contentsOfFile: path, encoding:NSUTF8StringEncoding)
+    
+    content = regex1.stringByReplacingMatchesInString(content,
+        options:NSMatchingOptions(rawValue: 0),
+        range: NSRange(location: 0, length: content.utf16.count), withTemplate:">")
+    
+    content = regex2.stringByReplacingMatchesInString(content,
+        options:NSMatchingOptions(rawValue: 0),
+        range: NSRange(location: 0, length: content.utf16.count), withTemplate:">")
+    
+    try content.writeToFile(path, atomically: true, encoding:NSUTF8StringEncoding)
+}
